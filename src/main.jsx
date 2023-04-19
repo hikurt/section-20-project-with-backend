b3a426ca6e55a0cf5fb9820bd9ebc6d3d6c096dd
hikurt <32220244+hikurt@users.noreply.github.com>
2023-04-19 11:07:29 +0200

    Dont work adding new event

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EventsPage, { loader as eventsLoader} from './pages/EventsPage';
 import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetailPage';
-import NewEventPage from './pages/NewEventPage';
+import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import RootLayout from './pages/RootLayout';
 import EventsRootLayout from './pages/EventsRootLayout';
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
         { index: true, element: <EventDetailPage /> },
         { path: 'edit', element: <EditEventPage /> },
       ]},
-      { path: 'new', element: <NewEventPage />},
+      { path: 'new', element: <NewEventPage/>, action: newEventAction},
     ]}
   ]
 },
